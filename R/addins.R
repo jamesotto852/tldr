b77054cc9bf27bdd091a1a92cb0e2794ea93e00f
jamesotto852 <jamesotto852@gmail.com>
2022-01-28 22:21:31 +0000

    First draft of addins. Would rather not require shiny, miniUI as imports -- see reprex

---
+++ R/addins.R
@@ -0,0 +1,48 @@
+tldr_selection_addin <- function() {
+  selection <- rstudioapi::selectionGet()
+  if (selection == "") {
+    return(invisible(NULL))
+  }
+  rstudioapi::sendToConsole(paste0("tldr::tldr(", selection, ")"), execute = TRUE, focus = FALSE)
+}
+
+
+# Very bare-bones miniUI shiny app
+# To-Do: Theming, change size
+tldr_input_addin <- function() {
+
+  # check_installed(c("shiny", "miniUI"), "in order to use the shiny addin")
+
+  # js for:
+  #   setting focus on load
+  #   hitting done on enter key press
+  js <- '
+$(document).on("shiny:connected", function(){
+  document.getElementById("topic").focus();
+});
+$(document).keyup(function(event) {
+    if ((event.key == "Enter")) {
+        document.getElementById("done").click();
+    }
+});
+'
+
+  ui <- miniUI::miniPage(
+    tags$head(tags$script(shiny::HTML(js))),
+    miniUI::gadgetTitleBar("tldr"),
+    miniUI::miniContentPanel(
+      shiny::textInput("topic", "Topic", value = "", width = NULL, placeholder = NULL)
+    )
+  )
+
+  server <- function(input, output, session) {
+    shiny::observeEvent(input$done, {
+      rstudioapi::sendToConsole(paste0("tldr::tldr(", input$topic, ")"), execute = TRUE, focus = FALSE)
+      shiny::stopApp()
+    })
+  }
+
+  app <- shiny::shinyApp(ui, server, options = list(quiet = TRUE))
+  shiny::runGadget(app, viewer = shiny::dialogViewer("tldr"))
+}
+
