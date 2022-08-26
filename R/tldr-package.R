2ecfd8c30d52fca1ae7e8102548e1b322d7bf7f6
jamesotto852 <jamesotto852@gmail.com>
2022-08-26 18:31:05 +0000

    Add tldr-package.R

---
+++ R/tldr-package.R
@@ -0,0 +1,10 @@
+#' tldr: Short-form documentation in the console
+#'
+#' A package that implements quick "tldr"-style documentation in the R console,
+#' providing the `tldr()` function for users and the roxygen2 extension
+#' `tldr-roclet` for package developers.
+#'
+#' @docType package
+#' @name tldr-package
+#' @ignoretldr
+NULL
